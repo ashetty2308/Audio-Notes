2a81109cd05d21ddae46e4153d1ccb66a749c0f9
Adit Shetty <ashetty2308@gmail.com>
2025-10-08 10:36:01 -0400

    removed more prints

--- server.py
+++ server.py
@@ -14,7 +14,6 @@ def get_bucket_key_filetype(url):
     # maxsplit = 1 here since we only want to split once into 2 buckets, key and file (could be nested file structure)
     bucket, key = postfix.split("/", 1)
     filetype = key.split('.', 1)[1]
-    print("WOOOO: ", bucket, key, filetype)
     return bucket, key, filetype
 
 class TextExtractionService(service_definition_pb2_grpc.TextExtractionServiceServicer):
@@ -23,7 +22,6 @@ class TextExtractionService(service_definition_pb2_grpc.TextExtractionServiceSer
         if not s3_url.startswith("s3://"):
             raise ValueError("Invalid url! (doesn't begin with correct s3:// path)")
         bucket, key, filetype = get_bucket_key_filetype(s3_url)
-        print(bucket, key, filetype)
         s3 = boto3.client('s3')
         file_bytes = s3.get_object(Bucket=bucket, Key=key)['Body'].read()
         with fitz.open(stream=BytesIO(file_bytes), filetype=filetype) as notes:
