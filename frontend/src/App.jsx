f7c13cd8c29c316cf5e4be1a07756ffb98c9652a
Adit Shetty <ashetty2308@gmail.com>
2025-10-12 12:02:57 -0400

    display all files in bucket via frontend; cleaned up gin router for CORS middleware and also edited response type for /get endpoint to be c.JSON to allow react/frontend to utilize it

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,11 +7,26 @@ function App() {
   const [file, setFile] = useState(null);
   const [audioURL, setAudioURL] = useState(null);
   const [loading, setLoading] = useState(null);
+  const [bucketFiles, setBucketFiles] = useState([]);
 
   
   const handleFileChange = (event) => {
     setFile(event.target.files[0]);
   }
+
+  useEffect(() => {
+    async function fetchFiles() {
+      try {
+        const res = await fetch("http://localhost:3000/bucket-files");
+        const data = await res.json();
+        console.log("Data from frontend: ", data);
+        setBucketFiles(data);
+      } catch (err) {
+        console.log("Error grabbing buckets from s3 bucket", err);
+      }
+    }
+    fetchFiles();
+  }, []);
 
   return (
     <div>
@@ -28,6 +43,12 @@ function App() {
             <audio controls src={audioURL} />
         </div>
       )}
+      <h3>Bucket Files</h3>
+      <ul>
+      {bucketFiles.map((file) => (
+        <li key={file.id}>{file.title}</li>
+      ))}
+    </ul>
     </div>
   )
 }
